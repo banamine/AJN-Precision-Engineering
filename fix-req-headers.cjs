c245efc1db549d894be9e715bd8977af2fa70291
Banamine <51272905+banamine@users.noreply.github.com>
2026-09-06 06:23:50 -0700

    chore: remove obsolete repair machinery fix-req-headers.cjs

--- fix-req-headers.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let server = fs.readFileSync('server.ts', 'utf8');
-
-server = server.replace(
-  'if (req.headers.range) headers.Range = req.headers.range;',
-  'if (req.headers.range) headers.Range = req.headers.range;\n  for (const h of ["if-range", "if-match", "if-none-match", "if-modified-since", "if-unmodified-since"]) {\n    if (req.headers[h]) headers[h] = req.headers[h];\n  }'
-);
-
-fs.writeFileSync('server.ts', server);
